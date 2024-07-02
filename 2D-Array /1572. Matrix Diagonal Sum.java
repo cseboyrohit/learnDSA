f5ec3a11cf2c0833635d1d63631e792e4c8efa1d
[ʀᴏʜɪᴛ ᴋᴜᴍᴀʀ] <147188266+cseboyrohit@users.noreply.github.com>
2024-07-02 09:15:02 +0530

    Update and rename 1572. Matrix Diagonal Sum to 1572. Matrix Diagonal Sum.java

--- 2D-Array /1572. Matrix Diagonal Sum.java
+++ 2D-Array /1572. Matrix Diagonal Sum.java
@@ -1,4 +1,4 @@
-Given a square matrix mat, return the sum of the matrix diagonals.
+/* Given a square matrix mat, return the sum of the matrix diagonals.
 
 Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
 
@@ -22,7 +22,7 @@ Example 3:
 Input: mat = [[5]]
 Output: 5
 
-****************************************************************************************
+**************************************************************************************** */
 
 
 class Solution {
