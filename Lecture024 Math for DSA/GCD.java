5552881b5da98726a71cfeb9ab69c14a7fc7b6f1
[ʀᴏʜɪᴛ ᴋᴜᴍᴀʀ] <147188266+cseboyrohit@users.noreply.github.com>
2024-07-03 22:10:19 +0530

    Update GCD.java

--- Lecture024 Math for DSA/GCD.java
+++ Lecture024 Math for DSA/GCD.java
@@ -10,6 +10,6 @@ public class GCD {
     static int gcd(int a, int b){
         if(a == 0)
             return b;
-        return gcd(b % a , a);
+        return gcd(b % a , a); //gcd(b, a%b);
     }
 }
