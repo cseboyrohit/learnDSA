c9bbd9992ddef4f1285ffa4026dd3ff8c9c19d00
[ʀᴏʜɪᴛ ᴋᴜᴍᴀʀ] <147188266+cseboyrohit@users.noreply.github.com>
2024-07-03 23:23:00 +0530

    Update SquareRootOfANumber.java

--- Lecture024 Math for DSA/SquareRootOfANumber.java
+++ Lecture024 Math for DSA/SquareRootOfANumber.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class SquareRootOfANumber {
     // Find Square Root of a Number Using Binary Search
     public static void main(String [] args){
-        // Creating Scanner Object for taking Input from the USER
         Scanner sc = new Scanner(System.in);
         // Take Any Number as a Input from the USER
         System.out.print(" Enter Any Number: ");
@@ -30,4 +29,34 @@ public class SquareRootOfANumber {
         // Otherwise return Floor Value of the Square Root
         return end;
     }
+}
+
+___________________________________________________________________________________
+
+    
+public class Main
+{
+    public static int squareRoot(int num){
+        int start = 0;
+        int end = num;
+        
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            
+            if(mid == num){
+                return mid;
+            }
+            else if(mid * mid > num){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return end;
+    }
+    
+	public static void main(String[] args) {
+		System.out.println(squareRoot(16));
+	}
 }
