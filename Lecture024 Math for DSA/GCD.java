4bd68383542273908678951be176cdc5a77d933f
[ʀᴏʜɪᴛ ᴋᴜᴍᴀʀ] <147188266+cseboyrohit@users.noreply.github.com>
2024-07-03 22:11:41 +0530

    Update GCD.java

--- Lecture024 Math for DSA/GCD.java
+++ Lecture024 Math for DSA/GCD.java
@@ -1,15 +1,21 @@
-package MathsForDSA;
+public class GCDExample {
 
-public class GCD {
-    // Find GCD of Two Numbers
-    public static void main(String [] args){
-        int a = 4;
-        int b = 9;
-        System.out.format(" Gcd of %d and %d is: %d ", a, b, gcd(a,b));
+    // Method to calculate GCD using Euclid's algorithm recursively
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
     }
-    static int gcd(int a, int b){
-        if(a == 0)
-            return b;
-        return gcd(b % a , a); //gcd(b, a%b);
+
+    public static void main(String[] args) {
+        int num1 = 36;
+        int num2 = 24;
+        
+        // Calculate GCD of num1 and num2
+        int result = gcd(num1, num2);
+        
+        // Output the result
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + result);
     }
 }
